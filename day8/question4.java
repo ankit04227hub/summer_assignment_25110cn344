411b4b661cddce0869a61182140db9d931f7322e
abhay <abhaykumarkushwahavaranasi@123gmail.com>
2026-06-09 17:27:49 +0530

    Initial commit

--- day8/question4.java
+++ day8/question4.java
@@ -1,14 +1,14 @@
 
-import java.util.Scanner;
+
 
 public class question4 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       
 
-        System.out.print("Enter number of rows: ");
-        int n = sc.nextInt();
+        
+        
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(i);
             }
