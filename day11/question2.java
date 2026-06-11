fe78a400ee99c78264deb339c128f6e2b22b72fd
abhay <abhaykumarkushwahavaranasi@123gmail.com>
2026-06-11 21:33:51 +0530

    First Commit

---
+++ day11/question2.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class question2 {
+
+    // Function to find maximum
+    static int max(int a, int b) {
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int result = max(num1, num2);
+
+        System.out.println("Maximum number = " + result);
+
+        sc.close();
+    }
+}
