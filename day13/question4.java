cd31017aacd1f4a0c9db667dcd73c525d6d2fad5
abhay <abhaykumarkushwahavaranasi@123gmail.com>
2026-06-13 20:31:16 +0530

    First Commit

---
+++ day13/question4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class question4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int evenCount = 0, oddCount = 0;
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+
+            if (arr[i] % 2 == 0)
+                evenCount++;
+            else
+                oddCount++;
+        }
+
+        System.out.println("Number of Even Elements = " + evenCount);
+        System.out.println("Number of Odd Elements = " + oddCount);
+
+        sc.close();
+    }
+}
